7846a3c622f51e1e307f6b40c05be6a2f6ee4bae
Callum Cameron <ccameron2@outlook.com>
2023-04-28 12:40:58 +0100

    Slime animations and Archways

--- Source/GamesSix/Archway.h
+++ Source/GamesSix/Archway.h
@@ -24,8 +24,10 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UFUNCTION()
 	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 
